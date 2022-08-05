ffb49260265416862a728ac869de65f3d4e43514
edmontheprogrammer <41594880+edmontheprogrammer@users.noreply.github.com>
2022-08-06 06:48:59 +0800

    @edmontheprogrammer
    Python Beginner to Advance Course Updates Commit

---
+++ Classes/Classes.py
@@ -0,0 +1,5 @@
+# Class: is a blueprint for creating new objects
+# Object: is an instance of a class
+
+# For example, we can have a class named "Human". Then create instance of it.
+# The following are objects of the 'Human' class, Objects: John, Mary, Jack
